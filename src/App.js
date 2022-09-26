d7107713b9ea7110969d18406fd44ee5b4740255
Kishanlal B J <kishanlalbj@gmail.com>
2022-09-26 19:05:23 +0530

    feat: added a counter app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React, { useState } from 'react';
 import Header from './components/Header';
 
 const App = () => {
-  const [a] = useState([]);
+  const [count, setCount] = useState(0);
 
-  console.log('error');
+  const handleDecrement = () => {
+    if (count > 0) {
+      setCount((prev) => prev - 1);
+    }
+  };
+
+  const handleIncrement = () => {
+    setCount((prev) => prev + 1);
+  };
 
   return (
     <div className="App">
       <Header />
+      <h1>{count}</h1>
+      <button onClick={handleIncrement}> Increment </button>
+      <button onClick={handleDecrement}> Decrement </button>
     </div>
   );
 };
